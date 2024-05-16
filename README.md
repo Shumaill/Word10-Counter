cd018ac8cf543d21149e18794225258bf67dc4aa
Shumaila Ali <160399825+Shumaill@users.noreply.github.com>
2024-05-16 01:25:25 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Word10-Counter" 
---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+//import the 'inquirer'module, which is a command line interface for Node.js
+import inquirer from 'inquirer';
+//Declare a constant 'answers'and assign it to the result of inquirer .prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Eneter yor sentence to count the word: "
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+//print the array of words to the console
+console.log(words);
+//print th eword count of the sentence to the co
+console.log(`Your Sentence word count is ${words.length}`);
